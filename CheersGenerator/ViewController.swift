1fdf254c12f0f37de7098271fed51afe2fa1ab3d
Rustam S <blakfoh@gmail.com>
2023-06-04 13:27:10 +0600

    Сделал генерацию тостов и поправил верстку

--- CheersGenerator/ViewController.swift
+++ CheersGenerator/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var whomLabel: UILabel!
+    @IBOutlet var iwhishLabel: UILabel!
+    @IBOutlet var beAndBecomeLabel: UILabel!
+    @IBOutlet var andLetLabel: UILabel!
+    
+    @IBOutlet weak var generateButton: UIButton!
+    @IBOutlet weak var copyButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setStyleButton()
     }
-
-
+        
+    // MARK: - Actions
+    @IBAction func generateButtonAction() {
+        whomLabel.text = whomPerson.randomElement()
+        iwhishLabel.text = iWhish.randomElement()
+        beAndBecomeLabel.text = beAndBecome.randomElement()
+        andLetLabel.text = andLet.randomElement()
+    }
+    
+    @IBAction func copyButtonAction() {
+    }
+    
 }
 
+extension ViewController {
+    func setStyleButton() {
+        generateButton.layer.cornerRadius = generateButton.frame.width / 2
+        copyButton.layer.cornerRadius = copyButton.frame.width / 2
+    }
+}
